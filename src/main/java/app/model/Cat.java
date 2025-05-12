70e122e71700846835f200a356fe6ed25b474cb9
Crimars <crimars@yandex.ru>
2025-05-12 23:51:05 +0300

    creat

--- src/main/java/app/model/Cat.java
+++ src/main/java/app/model/Cat.java
@@ -1,11 +1,11 @@
 package app.model;
-
 import org.springframework.stereotype.Component;
 
 @Component
 public class Cat extends Animal {
     @Override
-    public String toString() {
+    public String speak() {
         return "Im a Cat";
     }
+
 }
